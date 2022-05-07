e30dd628f978c35d04f75446f2f82d81bb15b039
romanown <qwerty012345@yandex.ru>
2022-05-08 00:47:43 +0300

    added eslint rules and prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,98 +1,99 @@
-import React, {useEffect, Children, useState} from 'react';
-import type {FC} from 'react';
-import { Routes, Route, Link } from "react-router-dom";
 import axios from 'axios';
+import type { FC } from 'react';
+import { Children, useEffect, useState } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 
-const App: FC = () =>  {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-            History App.
-        </p>
-      </header>
-	  <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="history" element={<HistoryPage />} />
-      </Routes>
-    </div>
-  );
-}
+const App: FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>History App.</p>
+    </header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="history" element={<HistoryPage />} />
+    </Routes>
+  </div>
+);
 
-const Home: FC = () =>  {
-  return (
-    <>
-      <main>
-        <h2>Welcome to the homepage!</h2>
-        <p>You can do this, I believe in you.</p>
-      </main>
-      <nav>
-        <Link to="/history">HistoryPage</Link>
-      </nav>
-    </>
-  );
-}
+const Home: FC = () => (
+  <>
+    <main>
+      <h2>Welcome to the homepage!</h2>
+      <p>You can do this, I believe in you.</p>
+    </main>
+    <nav>
+      <Link to="/history">HistoryPage</Link>
+    </nav>
+  </>
+);
 
 interface IEvent {
-    id: string;
-    appointmentId: string;
-    name: string;
-    resource: string;
-    date: string;
+  id: string;
+  appointmentId?: string;
+  name: string;
+  resource: string;
+  date: string;
 }
 
-interface IEvents {
-    items: IEvent[];
+interface IKey {
+  [key: string]: any;
 }
+
+const host: string | undefined = process.env.REACT_APP_BASE_URL;
 
 const api = axios.create({
-    baseURL: 'http://localhost:5010',
+  baseURL: host,
 });
 
-const HistoryPage: FC = () =>  {
-    const [events, setEvents] = useState<IEvent[]>([]);
-    useEffect(() => {
-        api
-            .get('/events')
-            .then((res) => {
-                const eventsArray = res?.data?.items || [];
-                setEvents(eventsArray);
-            })
-            .catch((error) => {
-                // eslint-disable-next-line no-console
-                console.log(error);
-            });
-    }, []);
+const HistoryPage: FC = () => {
+  const [events, setEvents] = useState<IEvent[] | IKey>([]);
+
+  const groupBy = (xs: IEvent[], key: string): IKey =>
+    xs.reduce((acc: IKey, x: IKey) => {
+      const intKey = x[key] || key;
+      (acc[intKey] = acc[intKey] || []).push(x);
+      return acc;
+    }, {});
+
+  useEffect(() => {
+    api
+      .get('/events')
+      .then((res) => {
+        const eventsArray = res?.data?.items || [];
+        setEvents(groupBy(eventsArray, 'noappointmentId'));
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.log(error);
+      });
+  }, []);
 
   return (
     <>
       <main>
         <h2>HistoryPage</h2>
-        <p>
-          That feels like an existential question, don't you
-          think?
-        </p>
+        <p>That feels like an existential question, don't you think?</p>
       </main>
       <nav>
         <Link to="/">Home</Link>
       </nav>
-        {Children.toArray(
-            events?.map((one:IEvent) => (
-<EventRow {...one} />
-                )))}
+      {Children.toArray(events?.map((one: IEvent) => <EventRow {...one} />))}
     </>
   );
-}
+};
 
-const EventRow: FC<IEvent> = props =>  {
-    const {id, name} = props;
-    return (
-        <div>
-            {id}
-            {name}
-        </div>
-    );
-}
+const EventRow: FC<IEvent> = (props) => {
+  const { id, name, appointmentId, resource, date } = props;
+  return (
+    <div>
+      {id}&nbsp;&nbsp;&nbsp;&nbsp;
+      {appointmentId || ''}&nbsp;&nbsp;&nbsp;&nbsp;
+      {name}&nbsp;&nbsp;&nbsp;&nbsp;
+      {resource}&nbsp;&nbsp;&nbsp;&nbsp;
+      {date}&nbsp;&nbsp;&nbsp;&nbsp;
+    </div>
+  );
+};
 
 export default App;

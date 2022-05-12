2d5aa9048e83ac3ebe6b80bb790475a4912c3b9c
romanown <qwerty012345@yandex.ru>
2022-05-12 09:34:42 +0300

    added styles to row

--- frontend/src/components/events/EventRow/index.tsx
+++ frontend/src/components/events/EventRow/index.tsx
@@ -1,17 +1,16 @@
 import type { FC } from 'react';
-
 import './styles.css';
-import type { IEvent } from './data';
+import type { IEvent } from 'types';
 
 const EventRow: FC<IEvent> = (props) => {
   const { id, name, appointmentId, resource, date } = props;
   return (
-    <div>
-      {id}&nbsp;&nbsp;&nbsp;&nbsp;
-      {appointmentId || '____________________'}&nbsp;&nbsp;&nbsp;&nbsp;
-      {name}&nbsp;&nbsp;&nbsp;&nbsp;
-      {resource}&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
-      {date}&nbsp;&nbsp;&nbsp;&nbsp;
+    <div className="event_row">
+      <div>{id}&nbsp;&nbsp;&nbsp;&nbsp;</div>
+      <div>{appointmentId || '____________________'}&nbsp;&nbsp;&nbsp;&nbsp;</div>
+      <div>{name}&nbsp;&nbsp;&nbsp;&nbsp;</div>
+      <div>{resource}&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;</div>
+      <div>{date}&nbsp;&nbsp;&nbsp;&nbsp;</div>
     </div>
   );
 };
